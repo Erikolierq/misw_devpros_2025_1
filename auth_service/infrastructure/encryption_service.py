4419e077a6e8993a5bcff85b6b806fee451e54c9
jhosalarcon <ja.alarcon2@uniandes.edu.co>
2025-03-07 12:05:26 -0500

    Decrypt password in auth_service

---
+++ auth_service/infrastructure/encryption_service.py
@@ -0,0 +1,16 @@
+from cryptography.fernet import Fernet
+import base64
+import os
+
+class EncryptionService:
+    def __init__(self):
+        key = os.environ.get('ENCRYPTION_KEY')
+        if not key:
+            raise ValueError("Falta la clave de encriptación en las variables de entorno")
+        self.cipher = Fernet(key.encode())
+
+    def encrypt(self, data):
+        return self.cipher.encrypt(data.encode()).decode()
+
+    def decrypt(self, encrypted_data):
+        return self.cipher.decrypt(encrypted_data.encode()).decode()
